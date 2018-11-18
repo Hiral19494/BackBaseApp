700491e03ee9d099ade92efff6278acc63e8af97
Hiral19494 <patelhiral19494@gmail.com>
2018-11-18 12:39:42 -0500

    Mapview showing marker

--- app/src/main/java/ca/android/backbaseapp/fragment/MapFragment.java
+++ app/src/main/java/ca/android/backbaseapp/fragment/MapFragment.java
@@ -1,20 +1,15 @@
 package ca.android.backbaseapp.fragment;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
@@ -23,7 +18,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import ca.android.backbaseapp.R;
 import ca.android.backbaseapp.model.City;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment  {
     private static final String TAG = CityFragment.class.getSimpleName();
     private static final String ARG_CITY = "city";
     private City mCity;
@@ -42,14 +37,6 @@ public class MapFragment extends Fragment {
         return mapFragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mCity = (City) getArguments().getSerializable(ARG_CITY);
-            Log.i(TAG, "mCity.getCityName() = " + mCity.getCityName());
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -57,38 +44,50 @@ public class MapFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_map, container, false);
         mapView = view.findViewById(R.id.map_view);
-        //mCity = (City) getArguments().get(ARG_CITY);
+        if (getArguments() != null) {
+            mCity = (City) getArguments().getSerializable(ARG_CITY);
+            Log.i(TAG, "mCity.getCityName() = " + mCity.getLatitude());
+        }
+
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                mGoogleMap = googleMap;
-                mGoogleMap.getUiSettings().setMyLocationButtonEnabled(false);
-                if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                mGoogleMap.setMyLocationEnabled(true);
-                MapsInitializer.initialize(getActivity());
-                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(mCity.getLatitude(), mCity.getLongitude()), 20);
-                mGoogleMap.animateCamera(cameraUpdate);
 
-                MarkerOptions optionOrigin = new MarkerOptions();
-                LatLng latLngOrigin = new LatLng(mCity.getLatitude(), mCity.getLongitude());
-                optionOrigin.position(latLngOrigin);
-                optionOrigin.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
-                mGoogleMap.addMarker(optionOrigin);
+                        googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+
+                        googleMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(mCity.getLatitude(), mCity.getLongitude()))
+                                .title("LinkedIn")
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
 
 
-            }
-        });
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mCity.getLatitude(), mCity.getLongitude()), 20));
+                    }
+                });
 
         return view;
     }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+
 }
